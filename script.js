ad4647d0384c0662591c0e607838eea6205a6e9f
Jura <Ladukeman@gmail.com>
2021-11-28 15:06:05 +0200

    homework

---
+++ script.js
@@ -0,0 +1,24 @@
+x=document.getElementById("test");
+x.addEventListener("click", function() {
+  document.getElementById("image").style.display = "none";
+  myfunction("trwereqwrrew");
+  });
+
+function myfunction() {
+    alert("Элемент был удалён")
+}
+
+
+
+
+
+
+document.getElementById("button").addEventListener("click",function() {
+  alert("fhvjckf");
+  document.getElementById("button").style.backgroundColor="red";
+  
+})
+
+
+
+
