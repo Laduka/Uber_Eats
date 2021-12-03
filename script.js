2994f1afc88059b5fbb72641c8c49e4af1feeb2a
Jura <Ladukeman@gmail.com>
2021-12-03 23:06:12 +0200

    Add aside block

--- script.js
+++ script.js
@@ -13,12 +13,19 @@ function myfunction() {
 
 
 
-document.getElementById("button").addEventListener("click",function() {
-  alert("fhvjckf");
-  document.getElementById("button").style.backgroundColor="red";
-  
+document.getElementById("test_button").addEventListener("click",function() {
+  inputTest = document.getElementById("test_input").value;
+  if ((inputTest.includes('@')) & inputTest.length > 9) {
+    alert('okv')
+  } else {
+    alert('ne ok')
+  }
 })
 
+names = ['t', 'f', 'a', 't', 'e', 'x']
 
+for(i=0; i < names.length; i++){
+  console.log(names[i]);
+}
 
 
